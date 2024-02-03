045dec5c1d6678470ed82ba2c16db707f6b1264d
apma2772 <apma2772@oulook.com>
2024-02-03 11:32:07 +0100

    Correct image paths for deployment

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
 
   let images = [];
   for (let i = 1; i <= 189; i++) {
-    images.push(`${process.env.PUBLIC_URL}/images/image${String(i).padStart(3, '0')}.png`);
-
+    images.push(`${process.env.PUBLIC_URL}/images/image${i}.png`);
   }
+  
 
 
   return (
